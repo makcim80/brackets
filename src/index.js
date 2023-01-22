d8730193a27a42f98bff024f4026ce4c35d38d2d
Makcim80 <makcim80@mail.ru>
2023-01-22 22:52:28 +0300

    feat: implement task in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let shield = '\\';
+  let bracketsArr = [];
+  let string = str;
+
+  if (+bracketsConfig[0][0]) {
+    shield = ''
+  }
+
+  bracketsArr = bracketsConfig.map(item => item.join(shield));
+
+  if (str.length % 2) {
+    return false
+  }
+
+  for (i = 0; i < str.length / 2; i++) {
+      bracketsArr.forEach(e => {
+          let re = new RegExp(shield + e)
+          string = string.replace(re, '')
+      })
+      if (!string) {
+        return true
+      }
+  }
+
+  return false
 }
